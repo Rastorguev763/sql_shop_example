481995295a1b227a7cdaf15d62d197bf0e6ab6dc
Rastorguev763 <117032024+Rastorguev763@users.noreply.github.com>
2023-07-10 17:19:51 +0300

    update

--- create_db.sql
+++ create_db.sql
@@ -28,7 +28,7 @@ CREATE TABLE PUBLIC.shop_dns (
   FOREIGN KEY (shop_id) REFERENCES public.shops (shop_id)
   );
  
- CREATE TABLE PUBLIC.shop_mvideo (
+CREATE TABLE PUBLIC.shop_mvideo (
   shop_id INT,
   date DATE,
   product_id INT,
@@ -37,7 +37,7 @@ CREATE TABLE PUBLIC.shop_dns (
   FOREIGN KEY (shop_id) REFERENCES public.shops (shop_id)
   );
   
-  CREATE TABLE PUBLIC.shop_sitilink (
+CREATE TABLE PUBLIC.shop_sitilink (
   shop_id INT,
   date DATE,
   product_id INT,
@@ -54,9 +54,9 @@ INSERT INTO PUBLIC.shops (shop_name) VALUES ('shop_sitilink');
 -- Добавление записей в таблицу products
 INSERT INTO PUBLIC.products (product_name, price)
 VALUES 
-('Испорченный телефон', 100.00),
-('Сарафанное радио', 500.00),
-('Патефон', 1000.00);
+  ('Испорченный телефон', 100.00),
+  ('Сарафанное радио', 500.00),
+  ('Патефон', 1000.00);
 
 -- Добавление записей в таблицу plan
 INSERT INTO PUBLIC.plan (product_id, shop_id, plan_cnt, plan_date)
@@ -70,3 +70,51 @@ VALUES
   (1, 3, 5, '2023-06-30'),
   (2, 3, 10, '2023-06-30'),
   (3, 3, 15, '2023-06-30');
+
+-- Добавление записей в таблицу shop_sitilink
+INSERT INTO PUBLIC.shop_sitilink (shop_id, date, product_id, sales_cnt)
+VALUES
+  (3, '2023-01-10', 1, 8),
+  (3, '2023-02-20', 2, 12),
+  (3, '2023-03-25', 3, 18),
+  (3, '2023-04-14', 1, 8),
+  (3, '2023-05-12', 2, 12),
+  (3, '2023-06-17', 3, 18),
+  (3, '2023-01-26', 1, 8),
+  (3, '2023-02-21', 2, 12),
+  (3, '2023-03-28', 3, 18),
+  (3, '2023-04-11', 1, 8),
+  (3, '2023-05-04', 2, 12),
+  (3, '2023-06-02', 3, 18),
+  (3, '2023-04-08', 1, 8),
+  (3, '2023-05-09', 2, 12),
+  (3, '2023-06-30', 3, 18),
+  (3, '2023-01-01', 1, 8),
+  (3, '2023-02-11', 2, 12),
+  (3, '2023-03-22', 3, 18);
+
+-- Добавление записей в таблицу shop_mvideo
+INSERT INTO PUBLIC.shop_mvideo (shop_id, date, product_id, sales_cnt)
+VALUES
+  (2, '2023-01-01', 1, 8),
+  (2, '2023-02-12', 2, 12),
+  (2, '2023-03-17', 3, 18),
+  (2, '2023-04-23', 1, 8),
+  (2, '2023-05-20', 2, 12),
+  (2, '2023-06-12', 3, 18),
+  (2, '2023-05-05', 2, 12),
+  (2, '2023-06-13', 3, 18),
+  (2, '2023-01-17', 1, 8),
+  (2, '2023-02-02', 2, 12),
+  (2, '2023-03-03', 3, 18);
+
+-- Добавление записей в таблицу shop_dns
+INSERT INTO PUBLIC.shop_dns (shop_id, date, product_id, sales_cnt)
+VALUES
+  (1, '2023-01-24', 1, 8),
+  (1, '2023-02-18', 2, 12),
+  (1, '2023-03-16', 3, 18),
+  (1, '2023-04-03', 1, 8),
+  (1, '2023-05-08', 2, 12),
+  (1, '2023-06-15', 3, 18),
+  (1, '2023-04-10', 1, 8);
